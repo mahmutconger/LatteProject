8b922cb7a82980acdaac5383b5f278fa62b9d147
Mahmut Can Conger <mahmutconger@gmail.com>
2025-05-21 17:41:48 +0300

    Kamera özelliği ve kütüphaneleri eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.fir.expressions.builder.buildFunctionCall
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -5,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.anlarsinsoftware.latteproje"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.anlarsinsoftware.latteproje"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -26,6 +28,10 @@ android {
             )
         }
     }
+    buildFeatures{
+        viewBinding =true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,7 +43,7 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
@@ -45,4 +51,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-view:1.3.0")
 }
--- app/src/main/java/com/anlarsinsoftware/latteproje/MainActivity.kt
+++ app/src/main/java/com/anlarsinsoftware/latteproje/MainActivity.kt
@@ -4,20 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
     }
     fun taramaPage(view:View){
-        startActivity(Intent(this,TaramaActivity::class.java))
+        startActivity(Intent(this, TaramaActivity::class.java))
     }
     fun urunlerPage(view:View){
         startActivity(Intent(this,UrunlerActivity::class.java))
